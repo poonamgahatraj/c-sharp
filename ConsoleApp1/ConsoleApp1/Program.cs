e6d6735ca6979763e4a3f3877c47cc4923240217
monika <monika>
2023-06-04 13:18:54 +0545

    datatypes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,19 @@ Console.WriteLine("hi this is my journey to c#");
 Console.WriteLine("my name is poonam");
 Console.WriteLine("enter a number");
 
+// datatypes
+int x = 10;
+Console.WriteLine(x);
+
+float y = 2.5f;
+Console.WriteLine(y);
+
+decimal z = 3.488m;
+Console.WriteLine(z);
+
+char a = 'A';
+Console.WriteLine(a);
+
+bool b = true;
+Console.WriteLine(b);
+
